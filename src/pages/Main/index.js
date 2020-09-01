0d68719f42cbb1deee70c13825d6bdc42718ba7b
Marcos Ferreira <marcosferreira.developer@gmail.com>
2020-09-01 17:11:40 -0300

    styled Main Page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import { Text } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
 import Header from '../../components/Header';
 import Tabs from '../../components/Tabs';
 
-import { Container } from './styles';
+import { Container, Content, Card, CardHeader, CardContent, Title, Description, CardFooter, Annotation } from './styles';
 
 export default function Main() {
   return (
     <Container>
       <Header />
+
+      <Content>
+        <Card>
+          <CardHeader>
+            <MaterialIcons name="attach-money" size={28} color="#666" />
+            <MaterialIcons name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$ 197.611,65</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Transferência de R$ 20,00 recebida de Diego Schell Fernandes hoje às 18:00h
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+
       <Tabs />
     </Container>
   );
