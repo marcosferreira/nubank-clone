3925e340bb18fd4e892c9a7569a01b760097e35e
Marcos Ferreira <marcosferreira.developer@gmail.com>
2020-09-03 10:00:40 -0300

    Tabs Animated

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -6,8 +6,15 @@ import { Container, TabsContainer, TabItem, TabText } from './styles';
 export default function Tabs({ translateY }) {
   return (
     <Container style={{
+      transform: [{
+        translateY: translateY.interpolate({
+          inputRange: [0, 380],
+          outputRange: [0, 30],
+          extrapolate: 'clamp',
+        }),
+      }],
       opacity: translateY.interpolate({
-        inputRange: [0, 150],
+        inputRange: [0, 380],
         outputRange: [1, 0],
         extrapolate: 'clamp',
       })
